18e200155532773b3e11dfcd4fdcf4deb8354f07
Salvio Furbino <salviof@gmail.com>
2024-04-28 03:07:43 -0300

    Atualizacao automática

--- src/main/java/br/org/coletivoJava/fw/api/erp/tarefas/TarefasClickup.java
+++ src/main/java/br/org/coletivoJava/fw/api/erp/tarefas/TarefasClickup.java
@@ -2,13 +2,11 @@ package br.org.coletivoJava.fw.api.erp.tarefas;
 
 import javax.inject.Qualifier;
 import com.super_bits.modulosSB.SBCore.modulos.erp.InfoReferenciaApiErp;
-import br.org.coletivoJava.fw.api.erp.tarefas.ERPTarefas;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
-import br.org.coletivoJava.fw.api.erp.tarefas.ItfERPTarefasService;
 
 @Qualifier
 @InfoReferenciaApiErp(tipoObjeto = ERPTarefas.class)
